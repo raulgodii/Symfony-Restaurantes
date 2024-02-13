7a83b6d81fb328d0af9d76432bbf57deed44e0ce
raulgodii <raulgodii13@gmail.com>
2024-02-13 15:16:54 +0100

    categorias

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -4,13 +4,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoriaRepository;
+
+use App\Entity\Categoria;
 
 class LandingPageController extends AbstractController
 {
 
     #[Route('/', name: 'landing_page')]
-    public function index(): Response
+    public function index(CategoriaRepository $categoriaRepository): Response
     {
-        return $this->render('home.html');
+        $categorias = $categoriaRepository->getCategorias();
+
+        return $this->render('home.html', [
+            'categorias' => $categorias,
+        ]);
     }
 }
